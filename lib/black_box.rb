a4e0419fe0d129ba52537276bbd32a354ecef69f
Jose Augusto <joseaugusto.881@outlook.com>
2023-06-24 20:24:32 -0300

    initial gem architecture

---
+++ lib/black_box.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "black_box/version"
+
+module BlackBox
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/black_box/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BlackBox
+  VERSION = "0.1.0"
+end
